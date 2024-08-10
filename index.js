f3cecfae33b66072be3dc277acbac55a06a2dfdc
Vasyl <vasyl.fedinchuk@gmail.com>
2024-08-10 02:29:06 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const a = 5
+let a
+let b
 
 function myFn() {
-  function innerFn() {
-    console.log(a)
-  }
-  innerFn()
+  let b
+  a = true
+  b = 10
+  console.log(b)
 }
 
 myFn()
+
+console.log(a)
+console.log(b)
