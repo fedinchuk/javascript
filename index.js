ca45c1923147a1c070a255a292376a99b211278a
Vasyl <vasyl.fedinchuk@gmail.com>
2024-08-12 21:32:16 +0200

    update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-setTimeout(function () {
-  console.log('Відкладене повідомлення')
-}, 3000)
+const myFunction = (a, b) => {
+  let c
+  a = a + 1
+  c = a + b
+  return c
+}
+
+console.log(myFunction(5, 3))
