b1fa4555fd29912fd3171c1edc726b13742db3e0
Vasyl <vasyl.fedinchuk@gmail.com>
2024-08-16 19:18:17 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function mulByFactor(value, multiplier = 3) {
-  return value * multiplier
-}
+const mulByFactor = function (value, multiplier = 3) {
+  return value * multiplier;
+};
 
-console.log(mulByFactor(10, 2))
-console.log(mulByFactor(5))
+console.log(mulByFactor(10, 2));
+console.log(mulByFactor(5));
