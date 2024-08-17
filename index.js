2f9b14cd5d73daa8b2bc465d5013f3de7e03a19e
Vasyl <vasyl.fedinchuk@gmail.com>
2024-08-17 18:52:32 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-function myFn(a) {
-  console.log(a);
-}
+const myArray = [1, 2, 3];
+console.log(myArray);
 
-const b = true;
-let c = 10;
-
-console.log(myFn(2 + 3));
-console.log(myFn(b));
-console.log(myFn(c = c + 1));
-console.log(myFn(c = c + 1;));
-console.log(myFn(let d));
+const myArray2 = new Array(1, 2, 3);
+console.log(myArray2);
