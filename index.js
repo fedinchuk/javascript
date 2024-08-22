20db979b376d0941cd972ddecc7c1cabaed5a8b2
Vasyl <vasyl.fedinchuk@gmail.com>
2024-08-22 19:57:40 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const myArray = [1, 2, 3];
 console.log(myArray);
 
-const newArray = myArray.map((el) => el * 3);
+const newArray = myArray.map((el) => {
+  el * 3
+});
 
 console.log(newArray);
 console.log(myArray);
