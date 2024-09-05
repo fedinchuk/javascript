2f23228052127ede9f40b216389ac532381cc511
Vasyl <vasyl.fedinchuk@gmail.com>
2024-09-05 23:15:19 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const value1 = 11
-const value2 = 25
+let value = 11;
+console.log(value >= 0 ? value : -value);
 
-value1 && value2
-  ? myFunction1(value1, value2)
-  : myFunction2()
-  
+value = -5;
+const res = value >= 0 ? value : -value;
+console.log(res);
