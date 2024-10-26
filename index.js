e32d502d47fdfb15619def96c14cf753b1f69fb9
Vasyl <vasyl.fedinchuk@gmail.com>
2024-10-26 12:31:23 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-class Comment {
-  constructor(text) {
-    this.text = text;
-    this.votesQty = 0;
-  }
-
-  upvote() {
-    this.votesQty += 1;
-  }
-
-  static mergeComments(first, second) {
-    return `${first} ${second}`;
+class NumbersArray extends Array {
+  sum() {
+    return this.reduce((el, acc) => (acc += el), 0);
   }
 }
+
+const myArray = new NumbersArray(2, 5, 7);
+
+console.log(myArray);
+myArray.sum();
